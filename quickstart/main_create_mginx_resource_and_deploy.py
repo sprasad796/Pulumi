b48dfc6fb08f1ec3e6c17cf9704b83fc6c09494f
sprasad796 <sprasad796.uplevel@gmail.com>
2025-05-07 12:07:23 -0400

    update the existing files

---
+++ quickstart/main_create_mginx_resource_and_deploy.py
@@ -0,0 +1,44 @@
+"""A Kubernetes Python Pulumi program"""
+
+import pulumi
+from pulumi_kubernetes.apps.v1 import Deployment
+from pulumi_kubernetes.core.v1 import Service
+
+config = pulumi.Config()
+is_minikube = config.require_bool("isMinikube")
+
+app_name = "nginx"
+app_labels = { "app": "nginx" }
+
+deployment = Deployment(
+    "nginx",
+    spec={
+        "selector": { "match_labels": app_labels },
+        "replicas": 1,
+        "template": {
+            "metadata": { "labels": app_labels },
+            "spec": { "containers": [{ "name": "nginx", "image": "nginx" }] }
+        },
+    })
+
+# Allocate an IP to the Deployment.
+frontend = Service(
+    app_name,
+    metadata={
+	"labels": deployment.spec["template"]["metadata"]["labels"],
+    },
+    spec={
+        "type": "ClusterIP" if is_minikube else "LoadBalancer",
+        "ports": [{ "port": 80, "target_port": 80, "protocol": "TCP" }],
+	"selector": app_labels,
+    })
+
+# When "done", this will print the piblic IP.
+result = None
+if is_minikube:
+    result = frontend.spec.apply(lambda v: v["cluster_ip"] if "cluster_ip" in v else None)
+else:
+    ingress = frontend.status.load_balancer.apply(lambda v: v["ingress"][0] if "ingress" in v else "output<string>")
+    result = ingress.apply(lambda v: v["ip"] if v and "ip" in v else (v["hostname"] if v and "hostname" in v else "output<string>"))
+
+pulumi.export("ip", result)
